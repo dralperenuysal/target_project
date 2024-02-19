a3504a72760da3d1ebea805b3ed01f0d4b3ed2ee
dralperenuysal <dralperenuysal@gmail.com>
2024-02-20 00:47:40 +0300

    COG categories plotted.

--- scripts/data_analysis/emapper_data_analysis.py
+++ scripts/data_analysis/emapper_data_analysis.py
@@ -1,26 +1,59 @@
+import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # Define the paths
+main_path = "output/eggnog"
 path_hi = "output/eggnog/hi_eggnog.emapper.annotations"
 path_pa = "output/eggnog/pa_eggnog.emapper.annotations"
 path_sa_mrsa = "output/eggnog/sa_mrsa_eggnog.emapper.annotations"
 
-# Define the function to read dataframes
-def eggnog_filtration(path):
+# Define the function to read and manipulate the dataframes
+def eggnog_filtration(path, main_path, prefix):
     import pandas as pd
     df = pd.read_csv(path, sep="\t", header='infer', skiprows=3)
     df = df[:-3]
     df = df[['#query_name', 'seed_ortholog_score', 'best_tax_level', 'Preferred_name', 'COG Functional cat.', 'eggNOG free text desc.']]
+    df = df.rename(columns= {'Preferred_name': 'name',
+                             'COG Functional cat.': 'cog',
+                             'eggNOG free text desc.': 'description'})
+    df.dropna(subset= ['cog', 'name'] ,inplace=True)
+    df.name.to_csv(f"{main_path}/{prefix}_eggnog.txt", sep='\n', index=False, header=False)
     return df
 
-hi = eggnog_filtration(path_hi)
-pa = eggnog_filtration(path_pa)
-sa_mrsa = eggnog_filtration(path_sa_mrsa)
+hi = eggnog_filtration(path_hi, main_path, "hi")
+pa = eggnog_filtration(path_pa, main_path, "pa")
+sa_mrsa = eggnog_filtration(path_sa_mrsa, main_path, "sa_mrsa")
 
-hi.dropna(subset= ['Preferred_name'], inplace= True)
-pa.dropna(subset= ['Preferred_name'], inplace= True)
-sa_mrsa.dropna(subset= ['Preferred_name'], inplace= True)
+### -------------- CREATE THE REPRODUCIBLE GRAPHIC MAKER -----------------
+def plot_categories(hi, column_name='cog', organism = "Bacteria"):
+    hi_cog = pd.DataFrame(hi[column_name].value_counts()[:15]).reset_index()
+    hi_cog.columns = ['COG Functional Category', 'count']
 
-hi['Preferred_name'].to_csv("output/eggnog/hi_eggnog.txt", sep = '\n', index = False, header = False)
-pa['Preferred_name'].to_csv("output/eggnog/pa_eggnog.txt", sep = '\n', index = False, header = False)
-sa_mrsa['Preferred_name'].to_csv("output/eggnog/sa_mrsa_eggnog.txt", sep = '\n', index = False, header = False)
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.set_xlabel('COG Functional Category', fontsize=15)
+    ax.set_ylabel('Count', fontsize=15)
+    plt.title(f"{organism} - COG Functional Category", fontsize=20)
 
-df = pd.read_csv("output/eggnog/hi_eggnog.emapper.annotations", sep = '\t', header = 'infer', skiprows = 3)
+    # sns.barplot kullanarak barplot çizdirme ve değerleri ekleyin
+    barplot = sns.barplot(data=hi_cog, x='COG Functional Category', y='count', palette='flare')
+
+    # Barların üzerine değerleri ekleyin
+    for index, row in hi_cog.iterrows():
+        barplot.text(index, row['count'], round(row['count'], 2), color='black', ha="center", fontsize=15)
+
+    # Eksen değerlerinin daha okunabilir olmasını sağlayın
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+
+    plt.tight_layout()  # Grafiğin düzgün sığdırılması için
+    plt.show()
+
+### -------------- PLOT THE COG DATA -----------------
+
+plot_categories(hi, column_name='cog', organism="H. influenzae")
+plot_categories(pa, column_name='cog', organism="P. aeuruginosa")
+plot_categories(sa_mrsa, column_name='cog', organism="MRSA")
+
+
