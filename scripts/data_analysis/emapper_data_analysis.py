d86f3450e9b15e74ae73c57ca465a0c2fd1c711e
dralperenuysal <dralperenuysal@gmail.com>
2024-02-19 23:02:34 +0300

    Eggnog data analysis.

--- scripts/data_analysis/emapper_data_analysis.py
+++ scripts/data_analysis/emapper_data_analysis.py
@@ -1,7 +1,26 @@
-import pandas as pd
+# Define the paths
+path_hi = "output/eggnog/hi_eggnog.emapper.annotations"
+path_pa = "output/eggnog/pa_eggnog.emapper.annotations"
+path_sa_mrsa = "output/eggnog/sa_mrsa_eggnog.emapper.annotations"
 
-emap = pd.read_csv("output/eggnog_web/sa_mrsa_eggnogweb/sa_mrsa_eggnogmapper.tsv", sep= '\t', header='infer')
+# Define the function to read dataframes
+def eggnog_filtration(path):
+    import pandas as pd
+    df = pd.read_csv(path, sep="\t", header='infer', skiprows=3)
+    df = df[:-3]
+    df = df[['#query_name', 'seed_ortholog_score', 'best_tax_level', 'Preferred_name', 'COG Functional cat.', 'eggNOG free text desc.']]
+    return df
 
-emap = emap[emap['COG_category'] != "-"]
-emap = emap.dropna(subset=['COG_category'])
-emap = emap.drop(columns = ['evalue', 'score'])
+hi = eggnog_filtration(path_hi)
+pa = eggnog_filtration(path_pa)
+sa_mrsa = eggnog_filtration(path_sa_mrsa)
+
+hi.dropna(subset= ['Preferred_name'], inplace= True)
+pa.dropna(subset= ['Preferred_name'], inplace= True)
+sa_mrsa.dropna(subset= ['Preferred_name'], inplace= True)
+
+hi['Preferred_name'].to_csv("output/eggnog/hi_eggnog.txt", sep = '\n', index = False, header = False)
+pa['Preferred_name'].to_csv("output/eggnog/pa_eggnog.txt", sep = '\n', index = False, header = False)
+sa_mrsa['Preferred_name'].to_csv("output/eggnog/sa_mrsa_eggnog.txt", sep = '\n', index = False, header = False)
+
+df = pd.read_csv("output/eggnog/hi_eggnog.emapper.annotations", sep = '\t', header = 'infer', skiprows = 3)
